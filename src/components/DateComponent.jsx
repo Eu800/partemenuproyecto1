b141ad27f34788f6344cd7a2acb4c1013d70d9ba
MaVity22 <mateos.viteri@gmail.com>
2023-08-29 02:20:14 -0500

    Nuevos cambios

--- src/components/DateComponent.jsx
+++ src/components/DateComponent.jsx
@@ -8,15 +8,27 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 export default function DateComponent(props) {
   const { label, onChange, className, value } = props;
-  const [valueDate, setValueDate]= useState();
+  const [valueDate, setValueDate] = useState();
+  const [IsValue, setIsValue] = useState(false);
 
   useEffect(() => {
     // Si value no es vacío
     if (value) {
       // Convierte el string de value en un objeto de tipo dayjs
       setValueDate(dayjs(value));
+      // Se cambia a true el estado del value
+      setIsValue(true);
+    } else {
+      // Si value es vacío, se cambia el estado de validez a false
+      setIsValid(false);
     }
   }, [value]);
+
+  const validValue = (value) => {
+    if (value) {
+      setIsValue(true);
+    }
+  }
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -24,7 +36,11 @@ export default function DateComponent(props) {
         <DatePicker label={label} className={className} onChange={date => {
           const formattedDate = date.toISOString().split('T')[0]; // Convert to YYYY-MM-DD format
           onChange(className, formattedDate); // Call the handler with field name and formatted date
-        }} value={valueDate}/>
+        }} value={valueDate} onError={() => !IsValue} slotProps={{
+          textField: {
+            helperText: !IsValue && "Este campo es obligatorio"
+          },
+        }} />
       </DemoContainer>
     </LocalizationProvider>
   );
