d7652ebc91e97fa7cc1e90123a697478b6ba8a31
MaVity22 <mateos.viteri@gmail.com>
2023-08-29 01:46:37 -0500

    Validaciones de campos para el CRUD

--- src/components/DateComponent.jsx
+++ src/components/DateComponent.jsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 export default function DateComponent(props) {
-  const { label, onChange, className } = props;
+  const { label, onChange, className, value } = props;
+  const [valueDate, setValueDate]= useState();
+
+  useEffect(() => {
+    // Si value no es vacío
+    if (value) {
+      // Convierte el string de value en un objeto de tipo dayjs
+      setValueDate(dayjs(value));
+    }
+  }, [value]);
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -13,7 +24,7 @@ export default function DateComponent(props) {
         <DatePicker label={label} className={className} onChange={date => {
           const formattedDate = date.toISOString().split('T')[0]; // Convert to YYYY-MM-DD format
           onChange(className, formattedDate); // Call the handler with field name and formatted date
-        }} />
+        }} value={valueDate}/>
       </DemoContainer>
     </LocalizationProvider>
   );
--- src/components/SuppliesCRUD.jsx
+++ src/components/SuppliesCRUD.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import '../css/SuppliesCRUD.css'
-// Elementos para registar insumos
 // Elementos para listar insumos en la tabla
-import { Table, TableContainer, TableHead, TableCell, TableBody, TableRow, Button, Modal, TextField, Box, Select, MenuItem } from '@mui/material';
+import {
+    Table, TableContainer, TableHead, TableCell, TableBody, TableRow, Button, Modal, TextField, Box, MenuItem
+} from '@mui/material';
 // Elementos para hacer CRUD
 import { Edit, Delete } from '@mui/icons-material';
 // Componentes para fecha
@@ -34,6 +35,34 @@ const SuppliesCRUD = () => {
         estado: '',
         fechaAdd: '',
     };
+
+    // Estado local para rastrear si los campos son obligatorios y están llenos
+    const [isRequiredFilled, setIsRequiredFilled] = useState({
+        codigo: true,
+        fabricante: true,
+        item: true,
+        tipo: true,
+        estado: true,
+    });
+
+    // Función para validar si los campos obligatorios están llenos
+    const validateRequiredFields = () => {
+        const requiredFields = ['codigo', 'fabricante', 'item', 'tipo', 'estado'];
+        const newIsRequiredFilled = { ...isRequiredFilled };
+
+        requiredFields.forEach(field => {
+            if (!itemSelect[field]) {
+                newIsRequiredFilled[field] = false;
+            } else {
+                newIsRequiredFilled[field] = true;
+            }
+        });
+
+        setIsRequiredFilled(newIsRequiredFilled);
+
+        return Object.values(newIsRequiredFilled).every(isFilled => isFilled);
+    };
+
     // Variables de bandera para abrir y cerrar la ventana modal
     const [modalInsert, setModalInsert] = useState(false);
     //--- Variable de bandera para cuando se desee editar ---
@@ -42,8 +71,6 @@ const SuppliesCRUD = () => {
     const [listaSupl, setListaSupl] = useState([]);
     // Variable de estado para el item seleccionado
     const [itemSelect, setItemSelect] = useState(initialState);
-    // Variable para almacenar el indice de un item a editar
-    const [indice, setIndice] = useState(0);
 
     // Función para controlar los campos de cada atributo
     const handleAtrSupp = (e) => {
@@ -55,10 +82,10 @@ const SuppliesCRUD = () => {
     }
 
     const handleDate = (name, value) => {
-        setItemSelect(estPrevio => ({
-            ...estPrevio,
+        setItemSelect(prevState => ({
+            ...prevState,
             [name]: value
-        }))
+        }));
     }
 
     const handleDeleteClick = (index) => {
@@ -69,7 +96,7 @@ const SuppliesCRUD = () => {
     };
 
     //-- Función para editar el implemento --
-    const  editSelect = () => {
+    const editSelect = () => {
         var editedList = [...listaSupl];
         // Se guarda el ID del elemento a editar
         const index = editedList.findIndex(antiguo => antiguo.codigo === itemSelect.codigo);
@@ -80,16 +107,17 @@ const SuppliesCRUD = () => {
             setListaSupl(editedList); // Actualizar la lista con la lista editada
             setModalEdit(!modalEdit);
             // Se vuelve a incializar la variable de estado itemSelect
-            setItemSelect(initialState); 
+            setItemSelect(initialState);
         }
     };
 
     // Función para agregar el nuevo implemento
     const addSupp = () => {
-        listaSupl.push(itemSelect);
-        setModalInsert(!modalInsert);
-        // Se vuelve a incializar la variable de estado nuevoSupp
-        setItemSelect(initialState); // Limpia los valores de nuevoSupp
+        if (validateRequiredFields()) {
+            listaSupl.push(itemSelect);
+            setModalInsert(!modalInsert);
+            setItemSelect(initialState);
+        }
     }
 
     // Funciones para eliminar un suplemento
@@ -100,11 +128,27 @@ const SuppliesCRUD = () => {
 
     // Función para controlar la ventana modal Insertar
     const handleAbrirCerrarInsertar = () => {
+        setItemSelect(initialState);
+        setIsRequiredFilled({
+            codigo: true,
+            fabricante: true,
+            item: true,
+            tipo: true,
+            estado: true,
+        });
         setModalInsert(!modalInsert);
     }
 
     // Función para controlar la ventana modal Insertar
     const handleAbrirCerrarEditar = () => {
+        setItemSelect(initialState);
+        setIsRequiredFilled({
+            codigo: true,
+            fabricante: true,
+            item: true,
+            tipo: true,
+            estado: true,
+        });
         setModalEdit(!modalEdit);
     }
 
@@ -121,22 +165,34 @@ const SuppliesCRUD = () => {
     const bodyInsertar = (
         <Box sx={styleModal}>
             <h1>Registrar nuevos items</h1>
-            <TextField label="Código" name='codigo' onChange={handleAtrSupp} type='number' />
+            <TextField label="Código" name='codigo' onChange={handleAtrSupp} type='number' error={!isRequiredFilled.codigo}
+                helperText={!isRequiredFilled.codigo && "Este campo es obligatorio"} />
             <br /><br />
-            <TextField label="Fabricante" name='fabricante' onChange={handleAtrSupp} />
+            <TextField label="Fabricante" name='fabricante' onChange={handleAtrSupp} error={!isRequiredFilled.fabricante}
+                helperText={!isRequiredFilled.fabricante && "Este campo es obligatorio"} />
             <br /><br />
-            {/*<TextField label="Item" name='item' onChange={handleAtrSupp} />*/}
-            <Select label="Item" name="item" onChange={handleAtrSupp}>
-                <MenuItem value="linterna">LINTERNA</MenuItem>
-                <MenuItem value="radio">RADIO</MenuItem>
-                <MenuItem value="comunicador">COMUNICADOR</MenuItem>
-                <MenuItem value="celular">CELULAR</MenuItem>
-            </Select>
-            <br /><br />
-            <TextField label="Tipo" name='tipo' onChange={handleAtrSupp} />
-            <br /><br />
-            <TextField label="Estado" name='estado' onChange={handleAtrSupp} />
-            <br /><br />
+            <TextField sx={{ m: 1, minWidth: 235 }} id="label-item" name='item' select label="Item" onChange={handleAtrSupp} error={!isRequiredFilled.item}
+                helperText={!isRequiredFilled.item && "Este campo es obligatorio"}>
+                <MenuItem value="LINTERNA">LINTERNA</MenuItem>
+                <MenuItem value="RADIO">RADIO</MenuItem>
+                <MenuItem value="COMUNICADOR">COMUNICADOR</MenuItem>
+                <MenuItem value="CELULAR">CELULAR</MenuItem>
+            </TextField>
+            <br />
+            <TextField sx={{ m: 1, minWidth: 235 }} id="label-tipo" name='tipo' select label="Tipo" onChange={handleAtrSupp} error={!isRequiredFilled.tipo}
+                helperText={!isRequiredFilled.tipo && "Este campo es obligatorio"}>
+                <MenuItem value="UTILERÍA">UTILERÍA</MenuItem>
+                <MenuItem value="COMUNICACIÓN">COMUNICACIÓN</MenuItem>
+                <MenuItem value="OTRO">OTRO</MenuItem>
+            </TextField>
+            <br />
+            <TextField sx={{ m: 1, minWidth: 235 }} id="label-tipo" name='estado' select label="Estado" onChange={handleAtrSupp} error={!isRequiredFilled.estado}
+                helperText={!isRequiredFilled.estado && "Este campo es obligatorio"}>
+                <MenuItem value="DISPONIBLE">DISPONIBLE</MenuItem>
+                <MenuItem value="EN USO">EN USO</MenuItem>
+                <MenuItem value="DAÑADO">DAÑADO</MenuItem>
+            </TextField>
+            <br />
             <DateComponent label="Fecha de adquisición" className='fechaAdd' onChange={handleDate} style={{ marginLeft: 'auto', marginRight: 'auto', width: '80%' }} />
             <br />
             <div align='right'>
@@ -150,23 +206,41 @@ const SuppliesCRUD = () => {
     const bodyEditar = (
         <Box sx={styleModal}>
             <h1>Editar item</h1>
-            <TextField label="Código" name='codigo' onChange={handleAtrSupp} type='number' value={itemSelect && itemSelect.codigo} />
+            <TextField label="Código" name='codigo' onChange={handleAtrSupp} type='number' value={itemSelect && itemSelect.codigo}
+                error={!isRequiredFilled.codigo} helperText={!isRequiredFilled.codigo && "Este campo es obligatorio"} />
             <br /><br />
-            <TextField label="Fabricante" name='fabricante' onChange={handleAtrSupp} value={itemSelect && itemSelect.fabricante} />
+            <TextField label="Fabricante" name='fabricante' onChange={handleAtrSupp} value={itemSelect && itemSelect.fabricante}
+                error={!isRequiredFilled.fabricante} helperText={!isRequiredFilled.fabricante && "Este campo es obligatorio"} />
             <br /><br />
-            <Select label="Item" name="item" onChange={handleAtrSupp} value={itemSelect && itemSelect.item}>
-                <MenuItem value="linterna">LINTERNA</MenuItem>
-                <MenuItem value="radio">RADIO</MenuItem>
-                <MenuItem value="comunicador">COMUNICADOR</MenuItem>
-                <MenuItem value="celular">CELULAR</MenuItem>
-            </Select>
+            <TextField sx={{ m: 1, minWidth: 235 }} id="label-item" name='item' select label="Item" onChange={handleAtrSupp} value={itemSelect && itemSelect.item}
+                error={!isRequiredFilled.item} helperText={!isRequiredFilled.item && "Este campo es obligatorio"}>
+                <MenuItem value="LINTERNA">LINTERNA</MenuItem>
+                <MenuItem value="RADIO">RADIO</MenuItem>
+                <MenuItem value="COMUNICADOR">COMUNICADOR</MenuItem>
+                <MenuItem value="CELULAR">CELULAR</MenuItem>
+            </TextField>
             <br /><br />
-            <TextField label="Tipo" name='tipo' onChange={handleAtrSupp} value={itemSelect && itemSelect.tipo} />
+            <TextField sx={{ m: 1, minWidth: 235 }} id="label-tipo" name='tipo' select label="Tipo" onChange={handleAtrSupp} value={itemSelect && itemSelect.tipo}
+                error={!isRequiredFilled.tipo} helperText={!isRequiredFilled.tipo && "Este campo es obligatorio"}>
+                <MenuItem value="UTILERÍA">UTILERÍA</MenuItem>
+                <MenuItem value="COMUNICACIÓN">COMUNICACIÓN</MenuItem>
+                <MenuItem value="OTRO">OTRO</MenuItem>
+            </TextField>
             <br /><br />
-            <TextField label="Estado" name='estado' onChange={handleAtrSupp} value={itemSelect && itemSelect.estado} />
+            <TextField sx={{ m: 1, minWidth: 235 }} id="label-estado" name='estado' select label="Estado" onChange={handleAtrSupp} value={itemSelect && itemSelect.estado}
+                error={!isRequiredFilled.estado} helperText={!isRequiredFilled.estado && "Este campo es obligatorio"}>
+                <MenuItem value="DISPONIBLE">DISPONIBLE</MenuItem>
+                <MenuItem value="EN USO">EN USO</MenuItem>
+                <MenuItem value="DAÑADO">DAÑADO</MenuItem>
+            </TextField>
             <br /><br />
-            <DateComponent label="Fecha de adquisición" className='fechaAdd' onChange={handleDate} 
-            style={{ marginLeft: 'auto', marginRight: 'auto', width: '80%' }} />
+            <DateComponent
+                label="Fecha de adquisición"
+                className="fechaAdd"
+                value={itemSelect.fechaAdd}
+                onChange={handleDate}
+                style={{ marginLeft: 'auto', marginRight: 'auto', width: '80%' }}
+            />
             <br />
             <div align='right'>
                 <Button color='primary' onClick={() => editSelect()}>Guardar</Button>
